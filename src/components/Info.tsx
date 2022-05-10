666e21bae6ef96d3e012d54822ddc58d07e16696
Justin <info@justinle.me>
2022-05-09 19:19:55 -0700

    initial about component

---
+++ src/components/Info.tsx
@@ -0,0 +1,21 @@
+export default function Info() {
+    return (
+        <div className="max-w-2xl mx-auto my-10">
+            <h1>Hey, thanks for visiting! 👋</h1>
+            <br></br>
+            <p>
+                I am a Full Stack Engineer based in <b>Seattle, WA</b>. I love
+                to design and create applications that look as great as they are
+                user-friendly and TypeScript is my favorite language. I am
+                currently taking on freelance projects and looking to contribute
+                to the success of a team/company that is striving to make an
+                impact on the world, no matter the size.
+            </p>
+            <br></br>
+            <p>
+                Outside of the office, you can find me biking or at the gym
+                working out!
+            </p>
+        </div>
+    );
+}
