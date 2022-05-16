8a04b3cdf4929e9a72daccc3fa003c2c8217218a
Justin <info@justinle.me>
2022-05-15 21:15:06 -0700

    added arrow, hover to repo button

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -126,14 +126,23 @@ export default function Projects(): JSX.Element {
                                 </div>
                             ))}
                         </div>
-                        <button className="rounded-lg bg-black text-white font-bold px-7 py-2">
-                            <a
-                                href={project.repo}
-                                target="_blank"
-                                rel="noreferrer"
+                        <button
+                            type="button"
+                            className="text-white bg-black hover:bg-[#1b1b15] font-bold rounded-lg text-sm px-5 py-2 inline-flex"
+                        >
+                            repo
+                            <svg
+                                className="w-5 h-5 ml-2 -mr-1"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
                             >
-                                repo
-                            </a>
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                    clip-rule="evenodd"
+                                ></path>
+                            </svg>
                         </button>
                     </div>
                 </div>
